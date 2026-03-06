9b668ba84a41f6c38a159ff46d2f8b2f6f865504
AdithyaVardan1 <adithyavardan.melam@gmail.com>
2026-03-07 02:42:45 +0530

    Clutter cleared in root folder

--- debug-smartapi.js
+++
@@ -1,4 +0,0 @@
-const { SmartAPI } = require('smartapi-javascript');
-const smartApi = new SmartAPI({ api_key: 'test' });
-console.log('Available methods:', Object.getOwnPropertyNames(Object.getPrototypeOf(smartApi)));
-console.log('Instance keys:', Object.keys(smartApi));
